a8819e660206c2843368311b16a16cde6162b275
zeropio <emilio.sanchez@outlook.es>
2023-12-20 09:03:39 +0100

    Update output

--- src/main.c
+++ src/main.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
 #include "common.h"
 
+void printHeader(const char *title) {
+    printf("\n=================================\n");
+    printf("     %s\n", title);
+    printf("=================================\n");
+}
+
 int main() {
 
     // Check CPUID availability
-    printf("Checking CPUID availability... ");
+    printHeader("Checking CPUID Availability");
+    printf("Status: ");
     if (!is_cpuid_supported()) {
-        printf("CPUID not supported.");
+        printf("\033[1;31mNot supported\033[0m\n");
         return -1;
     }
-    printf("Check!\n");
+    printf("\033[1;32mSupported\033[0m\n");
 
-    // Check if it's VM via CPUID
-    printf("Checking VM... ");
+    // Check if it's a VM via CPUID
+    printHeader("Checking Virtual Machine");
+    printf("Status: ");
     if (is_vm_running() == 1) {
-        printf("VM not detected.\n");
+        printf("\033[1;31mVirtual Machine detected\033[0m\n");
         return -1;
     }
-    printf("Check!\n");
+    printf("\033[1;32mNo Virtual Machine detected\033[0m\n");
 
     // Check vendors
+    printHeader("Checking Virtualization Vendor");
     switch (get_vm_vendor()) {
         case VENDOR_VMWARE:
-            printf("VMWare detected\n");
+            printf("\033[1;34mVMWare detected\033[0m\n");
             break;
         case VENDOR_VBOX:
-            printf("VirtualBox detected\n");
+            printf("\033[1;34mVirtualBox detected\033[0m\n");
             break;
         case VENDOR_QEMU:
-            printf("QEMU detected\n");
+            printf("\033[1;34mQEMU detected\033[0m\n");
             break;
         case VENDOR_WSL:
-            printf("WSL detected\n");
+            printf("\033[1;34mWSL detected\033[0m\n");
             break;
         case VENDOR_UNKNOWN:
-            printf("No known virtualization vendor detected\n");
+            printf("\033[1;33mNo known virtualization vendor detected\033[0m\n");
             break;
     }
 
